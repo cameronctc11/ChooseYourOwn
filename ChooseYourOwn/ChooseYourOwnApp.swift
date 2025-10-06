5d828ccbf5fa18ee0a91bdcd6e117e2e9b0fe4cb
cameronmckenzie <cameronctc11@gmail.com>
2025-10-06 13:26:34 -0600

    Initial Commit

---
+++ ChooseYourOwn/ChooseYourOwnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnApp.swift
+//  ChooseYourOwn
+//
+//  Created by McKenzie, Cameron - Student on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChooseYourOwnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChooseYourOwnTests/ChooseYourOwnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseYourOwnTests.swift
+//  ChooseYourOwnTests
+//
+//  Created by McKenzie, Cameron - Student on 10/6/25.
+//
+
+import Testing
+@testable import ChooseYourOwn
+
+struct ChooseYourOwnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
